8e3275ea32a4a7ca70e8f67734f92f87ea308c69
Yousef <202110410@students.asu.edu.jo>
2024-04-01 22:22:16 +0300

    This is the last commit

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -142,8 +142,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
     ],
-    #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    #'PAGE_SIZE' : 5,
+
     'DEFAULT_AUTHENTICATION_CLASSES' : [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -8,11 +8,10 @@ router: DefaultRouter = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('about/', views.about, name="about"),
     path('menu/', views.MenuItemsView.as_view(), name='menu'),
     path('reservations/', views.reservations, name="reservations"),
-    
     path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-item'),
     path('booking/', include(router.urls)),
 ]
