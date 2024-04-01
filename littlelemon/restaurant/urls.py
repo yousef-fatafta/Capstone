bd15bdf8734f38bef88618c7ba20e229f4c25574
Yousef <202110410@students.asu.edu.jo>
2024-04-01 21:43:33 +0300

    This is the seventh commit

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+
 from . import views
 
+from rest_framework.routers import DefaultRouter
+
+router: DefaultRouter = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name="about"),
+    path('menu/', views.MenuItemsView.as_view(), name='menu'),
+    path('reservations/', views.reservations, name="reservations"),
+    
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-item'),
+    path('booking/', include(router.urls)),
 ]
